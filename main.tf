865b9812aaec421ef2f3e223501c269599790343
Luis <lubeso@icloud.com>
2023-11-24 00:12:57 -0500

    Feature: Add GitHub OIDC identity provider

--- main.tf
+++ main.tf
@@ -27,3 +27,40 @@ module "terraform_cloud_oidc" {
   # Optional inputs
   # Nothing to do here...
 }
+
+module "github_actions_oidc" {
+  # Source configuration
+  source = "./modules/oidc"
+  # Required inputs
+  # Google Cloud provider configuration
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+  # Implementation variables
+  workload_identity_pool = {
+    id           = "github-actions"
+    display_name = "GitHub Actions"
+  }
+  workload_identity_pool_provider = {
+    attribute_condition = "(assertion.repository_owner_id == '${
+      var.github_owner_id
+      }' && assertion.repository_id == '${
+      var.github_repository_id
+    })'"
+    attribute_mapping = { "google.subject" = "assertion.ref" }
+    oidc              = { issuer_uri = "https://token.actions.githubusercontent.com" }
+  }
+  service_account = {
+    account_id   = "github-actions"
+    display_name = "GitHub Actions"
+    roles = [
+      "artifactregistry.admin",
+      "cloudbuild.builds.builder",
+      "run.admin",
+      "storage.admin",
+    ],
+    subject = "refs/heads/main"
+  }
+  # Optional inputs
+  # Nothing to do here...
+}
