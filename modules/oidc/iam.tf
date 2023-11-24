988b5fb6de91b2e23ba8ede6b1672b588bd58270
Luis <lubeso@icloud.com>
2023-11-23 23:59:24 -0500

    Fix: Refactor IAM implementation

--- modules/oidc/iam.tf
+++ modules/oidc/iam.tf
@@ -1,21 +1,19 @@
-module "service_account" {
-  # Source configuration
-  source  = "terraform-google-modules/service-accounts/google"
-  version = "~> 4.2.2"
+resource "google_service_account" "default" {
+  # Required arguments
+  account_id = "${var.service_account.account_id}-${random_id.suffix.hex}"
+  # Optional arguments
+  display_name = var.service_account.display_name
+}
+
+resource "google_project_iam_member" "default" {
+  # Meta-arguments 
+  for_each = toset(
+    concat(var.service_account.roles, ["iam.serviceAccountUser"])
+  )
   # Required inputs
-  project_id = var.project
-  # Optional inputs
-  prefix       = random_id.prefix.hex
-  names        = [var.workload_identity_pool.id]
-  display_name = var.workload_identity_pool.display_name
-  project_roles = [
-    for role in distinct(
-      concat(var.service_account.roles, [
-        "iam.serviceAccountUser",
-        "iam.workloadIdentityUser"
-      ])
-    ) : "${var.project}=>roles/${role}"
-  ]
+  project = var.project
+  role    = "roles/${each.key}"
+  member  = "serviceAccount:${google_service_account.default.email}"
 }
 
 resource "google_service_account_iam_member" "workload_identity_user" {
